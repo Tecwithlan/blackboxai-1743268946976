b5d4d99453ec71c8bd29ac14f1b9bee442fcda74
Tecwithlan <masiboallan339@gmail.com>
2025-03-29 20:22:45 +0300

    Add construction_ecom/users/serializers.py 1743268963118

---
+++ construction_ecom/users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'business_name', 
+                 'business_type', 'tax_exempt', 'approved']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            business_name=validated_data['business_name'],
+            business_type=validated_data['business_type']
+        )
+        return user
